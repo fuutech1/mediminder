ff686eea48bd939e121083420743220f34f8ca7b
techxninjas <aadillatif95230@gmail.com>
2025-06-30 17:33:14 +0530

    Added bolt image

--- src/components/BoltBadge.tsx
+++ src/components/BoltBadge.tsx
@@ -9,7 +9,7 @@ const BoltBadge: React.FC = () => {
       className="fixed bottom-4 right-4 z-50"
     >
       <img
-        src="https://bolt.new/badge/built-with-bolt-dark.svg"
+        src="https://raw.githubusercontent.com/fuutech1/mediminder/b03be47b9dd1bbef3f0812240fe937982c885d58/badges/bolt-badge/black_circle_360x360/black_circle_360x360.svg"
         alt="Built with Bolt.new"
         className="w-28 hover:scale-105 transition-all"
       />
